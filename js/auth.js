8e613511e2c555478ab7702b412a4e10f63ef76d
KarolainReyes <reyeskarol0412@gmail.com>
2025-11-01 23:18:07 -0500

    feat: :sparkles: Add new fields in auth.js Expanded auth.js with new validation and data handling fields. by @KarolainReyes

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  const navbar = document.querySelector(".navbar");
+  const navbar = document.querySelector(".navita");
   const currentPage = window.location.pathname.split("/").pop(); 
 
   try {
@@ -20,12 +20,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     let newButton = document.createElement("button");
     if (usuario.rol === "Admin") {
       newButton.textContent = "Gestión";
+      newButton.classList.add("boton-dinamico")
       // Redirigir correctamente desde cualquier página
       newButton.addEventListener("click", () => {
         window.location.href = currentPage === "index.html" ? "./html/admin.html" : "../html/admin.html";
       });
     } else {
       newButton.textContent = "Mi perfil";
+      newButton.classList.add("boton-dinamico")
       newButton.addEventListener("click", () => {
         window.location.href = currentPage === "index.html" ? "./html/perfil_user.html" : "../html/perfil_user.html";
       });
